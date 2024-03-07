dd755641f5c1b6013a215126f515743baae3c626
Mohammed Sadique <sadiquemohd48@gmail.com>
2024-03-08 01:23:24 +0530

    qwerty

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './Main.css';
 import aboutImage from './images/About.svg';
+import mainImage from './images/main.png';
 import ruleImage from './images/Rules.png';
 import faqImage from './images/Faq.png';
 import timeImage from './images/Timing.png';
@@ -56,31 +57,30 @@ const Main = () => {
                     </ul>
                 </div>
             </main>
+
+
+
             <section className="text-white text-center " id="About">
                 <h2>About us</h2>
                 <p>
-                    CodeChef ADGIPS & ECELL ADGIPS are
-                    dynamic communities of tech
-                    enthusiasts and entrepreneurship
-                    enthusiasts who are committed to
-                    promoting creativity and problem solving.
-                    <br /><br /> We are revolutionizers and with
-                    the power of our ideas intend to bring
-                    about a positive change in the world
-                    that would make it a better place for
-                    everyone to live in. Constantly
-                    developing and building ourselves is our
-                    motto. <br /><br />
-                    Everyone can materialize what
-                    they fantasize about, it’s just a matter
-                    of opening one’s eyes and putting in the
-                    right amount of faith and hard work.
-                    Our shared goal is to inspire meaningful
-                    impact and innovation, creating a
-                    transformative space where technology
-                    meets empowerment
+                CodeChef ADGIPS & ECELL ADGIPS are dynamic communities of tech enthusiasts and entrepreneurship enthusiasts who are committed to promoting creativity and problem solving.
+                <br /><br />
+                We are revolutionizers and with the power of our ideas intend to bring about a positive change in the world that would make it a better place for everyone to live in. Constantly developing and building ourselves is our motto.
+                <br /><br />
+                Everyone can materialize what they fantasize about, it’s just a matter of opening one’s eyes and putting in the right amount of faith and hard work. Our shared goal is to inspire meaningful impact and innovation, creating a transformative space where technology meets empowerment
                 </p>
             </section>
+
+            <section className="text-white text-center " id="Event">
+                <h2>Event</h2>
+                <img src={mainImage} alt="CodeChefXEcell" />
+                <p>
+                Innoveda is a launchpad for startup minded innovators and tech enthusiasts to create disruptive solutions that address real-world problems.
+                </p>
+            </section>
+
+
+
             <footer className="text-center bg-black" id="Contact">
                 <h4>Contact Information</h4>
                 <span>Reach out to us for any inquiries or support.</span><br /><br />
